c3e389ba990b142c35bda47a3cab550dfa78f307
raj <tiwaridevesh419@gmail.com>
2023-12-16 13:40:56 +0530

    commited

--- Desktop/fame tatto/Fame-api-tatoo-main/Login_client/login/loginapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, UserLoginView
-
-app_name = 'loginapp'
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-]
--- Desktop/fame_api/User/UserModel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsermodelConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "UserModel"
